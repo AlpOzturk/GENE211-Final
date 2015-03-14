d6f15a3135d9b67579ea3801943da5b70948aaa9
Alp Ozturk <alp.t.ozturk@gmail.com>
2015-03-14 03:18:14 -0700

    First version of parser

---
+++ analyze_associations.py
@@ -0,0 +1,11 @@
+
+import sys
+from dis_gen_net_parser import parseDisGenNet
+
+ALL_FILE_NAME = "all_gene_disease_associations.txt"
+
+def run():
+    associations = parseDisGenNet(ALL_FILE_NAME)
+    print associations
+    
+run()
---
+++ dis_gen_net_parser.py
@@ -0,0 +1,18 @@
+
+DELIMITER = "\t" 
+GENE_INDEX = 1
+DISEASE_INDEX = 4
+SCORE_INDEX = 5
+
+def parseDisGenNet(inputFileName):
+    inputFile = open(inputFileName, "r")
+    associations = set()
+    isFirst = True
+    for line in inputFile:
+        if isFirst:
+            isFirst = False
+        else:
+            splitLine = line.strip().split(DELIMITER)
+            association = (splitLine[GENE_INDEX], splitLine[DISEASE_INDEX], float(splitLine[SCORE_INDEX]))
+            associations.add(association)
+    return associations
