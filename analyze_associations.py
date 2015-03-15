7e126f7ba9beabab00dcbcc4441958a14fde0e9f
Alp Turhan Ozturk <aozturk@corn36.stanford.edu>
2015-03-15 05:38:43 -0700

    Perliminary gene analysis online

--- analyze_associations.py
+++ analyze_associations.py
@@ -36,51 +36,18 @@ def run():
         geneList = geneList.union(disorderMap[disorder])
     geneList = list(geneList)
     geneList.sort()
+    print "Performing Heirarchical Clustering ..."
     performHeirarchicalClustering(geneList, disorderMap, scoreMap)
+    print "Conducting genetic analysis ..."
+    conductGeneAnalysis(geneList, disorderMap)
 
-
-def performHeirarchicalClustering(geneList, disorderMap, scoreMap):
-    dataMatrix = list() 
-    disorderList = list(disorderMap.keys())
-    disorderList.sort()
-    for disorder in disorderList:
-        dataMatrix.append([scoreMap.get((disorder, gene), 0.0) for gene in geneList])
-    dataMatrix = numpy.array(dataMatrix)
-    distanceMatrix = dist.squareform(dist.pdist(dataMatrix))
-    linkageMatrix = hier.linkage(distanceMatrix)
-    dendrogram = hier.dendrogram(linkageMatrix, labels=disorderList, leaf_font_size = LEAF_FONT_SIZE)
-    #leaves = dendrogram["leaves"]
-    #reorderedData = dataMatrix[leaves,:]
-    pylab.savefig( "diseaseDentrogram.png" )
-    pylab.cla()
-
-#get your data into a 2d array where rows are genes, and columns 
-#are conditions
-#data = numpy.array(dataMatrix)
-
-#calculate a distance matrix
-#distMatrix = dist.pdist(data)
-
-#convert the distance matrix to square form. The distance matrix 
-#calculated above contains no redundancies, you want a square form 
-#matrix where the data is mirrored across the diagonal.
-# distSquareMatrix = dist.squareform(distMatrix)
-
-#calculate the linkage matrix 
-#linkageMatrix = hier.linkage(distSquareMatrix)
-
-#dendro = hier.dendrogram(linkageMatrix)
-
-#get the order of rows according to the dendrogram 
-#leaves = dendro['leaves'] 
-
-#reorder the original data according to the order of the 
-#dendrogram. Note that this slice notation is numpy specific.
-#It just means for every row specified in the 'leaves' array,
-#get all the columns. So it effectively remakes the data matrix
-#using the order from the 'leaves' array.
-#transformedData = data[leaves,:]
-
+def conductGeneAnalysis(geneList, disorderMap):
+    resultList = list()
+    for gene in geneList:
+        disorders = [disorder for disorder in disorderMap if gene in disorderMap[disorder]]
+        resultList.append((gene, len(disorders)))
+    resultList = sorted(resultList,key=lambda x: x[1])
+    print resultList
 
 def getTargetDisorderMap():
     inputFile = open(TARGET_FILE_NAME, "r")
@@ -107,5 +74,20 @@ def processAssociations(associations, regexMap):
                     scoreMap[scoreMapKey] = max(scoreMap.get(scoreMapKey, 0.0), score)
                     break
     return disorderMap, scoreMap
+
+def performHeirarchicalClustering(geneList, disorderMap, scoreMap):
+    dataMatrix = list() 
+    disorderList = list(disorderMap.keys())
+    disorderList.sort()
+    for disorder in disorderList:
+        dataMatrix.append([scoreMap.get((disorder, gene), 0.0) for gene in geneList])
+    dataMatrix = numpy.array(dataMatrix)
+    distanceMatrix = dist.squareform(dist.pdist(dataMatrix))
+    linkageMatrix = hier.linkage(distanceMatrix)
+    dendrogram = hier.dendrogram(linkageMatrix, labels=disorderList, leaf_font_size = LEAF_FONT_SIZE)
+    #leaves = dendrogram["leaves"]
+    #reorderedData = dataMatrix[leaves,:]
+    pylab.savefig( "diseaseDentrogram.png" )
+    pylab.cla()
 
 run()
